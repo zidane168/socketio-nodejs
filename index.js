deed8619b972d868911546045d959ed703b6daa2
ViLH <vi.lh@>
2022-04-26 16:36:05 +0700

    the number of event socket.broadcast.emit

--- index.js
+++ index.js
@@ -17,12 +17,36 @@ io.on("connection", function(socket) {
 
   socket.on("client-send-data", function(data) {
     console.log(`${data}`);
+
+    // send all client receive (include myself)
+    // io.sockets.emit("server-response-data", data);
+
+    // who send, who will receive
+    // socket.emit("server-response-data", data);
+
+    // send all client receive (exclude myself)
+    socket.broadcast.emit("server-response-data", data);
   })
 
   socket.on("client-send-object", function(data) {
-
     console.log(JSON.parse(data));
 
+    io.sockets.emit("server-response-object", data)
+
+    // send to socket id want to send
+    // io.to("socketid").emit();
+  })
+
+  socket.on("typing-event", function() {
+    const data = socket.id + " is typing!";
+    console.log(data)
+    socket.broadcast.emit("server-response-typing-event", data);
+  })
+
+  socket.on("stop-typing-event", function() {
+    const data = socket.id + " is stop!";
+    console.log(data)
+    socket.broadcast.emit("server-response-stop-typing-event", data);
   })
 })
 
